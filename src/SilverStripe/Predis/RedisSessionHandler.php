442dc40c58aa1d948fdb5773542dd0d1f0103648
Martijn van Nieuwenhoven <info@axyrmedia.nl>
2016-04-28 18:22:01 +0200

    update formatting...

--- src/SilverStripe/Predis/RedisSessionHandler.php
+++ src/SilverStripe/Predis/RedisSessionHandler.php
@@ -3,12 +3,9 @@
  * RedisSessionHandler
  * Use Redis to store sessions, so we can access them with for example node.js.
  * The session data is stored as json for easier parsing with other clients.
- *
  * Some code borrowed from http://www.sitepoint.com/saving-php-sessions-in-redis/
- *
- * Redis keys are stored as path:prefix:sessid, 
+ * Redis keys are stored as path:prefix:sessid,
  * but you can pick a key value pretty much whatever you want.
- *
  * Requires predis/predis
  * https://github.com/nrk/predis
  */
@@ -16,97 +13,106 @@ namespace SilverStripe\Predis;
 
 use Predis\Client;
 
-class RedisSessionHandler {
-    
-	public $client;
-	
-	public $ttl		= 1800;
-    
-	public $path	= 'sessions';
-	
-    public $prefix	= 'PHPSESSID';
- 	
-	public $key_sep	= ':';
- 
-    public function __construct($path = 'sessions', $prefix = 'PHPSESSID', $key_sep = ':') {
-        $this->client	= new Client();
-		$this->ttl		= ini_get('session.gc_maxlifetime');
-		
-		$this->path		= $path;
-		$this->prefix	= $prefix;
-		$this->key_sep	= $key_sep;
-		
-		session_set_save_handler(
-			array($this, "open"),
-			array($this, "close"),
-			array($this, "read"),
-			array($this, "write"),
-			array($this, "destroy"),
-			array($this, "gc")
-		);
+class RedisSessionHandler
+{
+
+    public $client;
+
+    public $ttl = 1800;
+
+    public $path = 'sessions';
+
+    public $prefix = 'PHPSESSID';
+
+    public $key_sep = ':';
+
+    public function __construct($path = 'sessions', $prefix = 'PHPSESSID', $key_sep = ':')
+    {
+        $this->client = new Client();
+        $this->ttl    = ini_get('session.gc_maxlifetime');
+
+        $this->path    = $path;
+        $this->prefix  = $prefix;
+        $this->key_sep = $key_sep;
+
+        session_set_save_handler(
+            [$this, "open"],
+            [$this, "close"],
+            [$this, "read"],
+            [$this, "write"],
+            [$this, "destroy"],
+            [$this, "gc"]
+        );
     }
-	
-	function redisKeyPath() {
-		return $this->path . $this->key_sep . $this->prefix . $this->key_sep;
-	}
-	
- 	/**
-	 * No action necessary because connection is injected
+
+    function redisKeyPath()
+    {
+        return $this->path . $this->key_sep . $this->prefix . $this->key_sep;
+    }
+
+    /**
+     * No action necessary because connection is injected
      * in constructor and arguments are not applicable.
      */
-	public function open($savePath, $sessionName) {
-		
+    public function open($savePath, $sessionName)
+    {
+
     }
- 
-    public function close() {
-        $this->client = NULL;
+
+    public function close()
+    {
+        $this->client = null;
         unset($this->client);
     }
- 
- 	/**
-	 * Gets json_encoded Session data from Redis
-	 * and encode it back to php's session encoding
-	 */
-    public function read($id) {
-		
-		$tmp = $_SESSION;
-		$id = $this->redisKeyPath() . $id;
-		
+
+    /**
+     * Gets json_encoded Session data from Redis
+     * and encode it back to php's session encoding
+     */
+    public function read($id)
+    {
+
+        $tmp = $_SESSION;
+        $id  = $this->redisKeyPath() . $id;
+
         $_SESSION = json_decode($this->client->get($id), true);
-		$this->client->expire($id, $this->ttl);
-		
-        if(isset($_SESSION) && !empty($_SESSION) && $_SESSION != NULL){
+        $this->client->expire($id, $this->ttl);
+
+        if (isset($_SESSION) && !empty($_SESSION) && $_SESSION != null) {
             $new_data = session_encode();
             $_SESSION = $tmp;
             return $new_data;
-        }else{
+        } else {
             return "";
         }
     }
- 	
-	/**
-	 * Writes Session json_encoded, so we can access the Session data with other clients like node.js
-	 */
-    public function write($id, $data) {
-		$tmp = $_SESSION;
+
+    /**
+     * Writes Session json_encoded, so we can access the Session data with other clients like node.js
+     */
+    public function write($id, $data)
+    {
+        $tmp = $_SESSION;
         session_decode($data);
         $new_data = $_SESSION;
         $_SESSION = $tmp;
-		
+
         $id = $this->redisKeyPath() . $id;
         $this->client->set($id, json_encode($new_data));
         $this->client->expire($id, $this->ttl);
-		return true;
+        return true;
     }
- 
-    public function destroy($id) {
+
+    public function destroy($id)
+    {
         $this->client->del($this->redisKeyPath() . $id);
     }
 
-	/**
-	 * No need to gc because of using expiration
-	 */
-    public function gc($maxLifetime) {
+    /**
+     * No need to gc because of using expiration
+     */
+    public function gc($maxLifetime)
+    {
 
     }
 }
